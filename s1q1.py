8fe4c6b8a3c5bf581f0a3f965ad75bafb8ac96de
fazrul <falzr@falzr_crosshair.localdomain>
2021-11-26 09:38:43 +0800

    section 1 question 1

--- s1q1.py
+++ s1q1.py
@@ -1,11 +1,8 @@
-# Using NO BUILT IN LIBRARIES, write a program that satisfies the following problem:
-# n! means n × (n − 1) × ... × 3 × 2 × 1
-# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800, and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
-# Find the sum of the digits in the number 100!
-def multiplication(n): # n =10
+# Find the sum of the digits.
+def multiplication(n):
   og = n
   while n > 1:
-    mult = og * (n-1) # 90 = 10 x 9
+    mult = og * (n-1)
     og = mult
     n -= 1
   return addition(mult)
@@ -14,4 +11,4 @@ def addition(i):
   add = sum([int(a) for a in str(i)])
   return add
 
-print(multiplication(10))
+print(multiplication(100))
